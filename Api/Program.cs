835417f73e9e2072bdd0bd9ad746784f67535738
John Kerr <kerr.john@gmail.com>
2024-09-26 21:40:41 +0100

    Remove boilerplate

--- Api/Program.cs
+++ Api/Program.cs
@@ -43,26 +43,6 @@ app.UseHangfireDashboard();
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
 app.MapGet("/test", (IPublishEndpoint endpoint) =>
     {
         endpoint.Publish(new TestMessage(Guid.NewGuid()));
@@ -72,7 +52,3 @@ app.MapGet("/test", (IPublishEndpoint endpoint) =>
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
